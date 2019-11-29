b606ebb012647641150c85bec76ef17268acab79
gsaied <gamal.saiedeece@gmail.com>
2019-11-29 18:21:42 +0200

    added changes to mac

---
+++ new_mac/synth.tcl
@@ -0,0 +1,9 @@
+set dirt /home/gsaied/Desktop/verilog_rtl/new_mac
+cd $dirt
+read_verilog  mac.v 
+#synth_design -top mac -rtl
+synth_design -top mac -part xc7vx690t -max_dsp 1
+report_timing -max_paths 50 -file $dirt/timing.rpt
+report_design_analysis -file $dirt/design.rpt
+report_utilization -file $dirt/utiliziation.rpt
+
