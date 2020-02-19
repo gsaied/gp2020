ec86cbcee1f1421afbcd230f35e9897e06ea9ac0
gsaied <gamal.saiedeece@gmail.com>
2020-02-20 00:34:48 +0200

    added fire9 and fire5 squeezes

---
+++ fire5_squeeze/move_remainder.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+file=1
+count=1
+find *.mem | grep extend | while read ss ; do
+	rm -rf $ss ;
+done
+echo -e "enter remainder (law enta m3ak 768 filters masalan enter 512)" ; 
+#actually msh remainder bs will do for now
+read remainder
+find *.mem | grep -v bias | grep -v weights | sort -n -t _ -k 2 | while read LINE ; do
+	cat $LINE | while read line2 ; do
+		if [[   "$count"  -gt  "$remainder"   ]] ; then
+			echo $line2 >> extend_"$layer_name"_"$LINE" ; 
+		fi
+			((count++))
+	done	
+	((file++));
+done
