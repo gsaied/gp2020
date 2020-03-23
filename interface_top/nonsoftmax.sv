115c53897cfc8b966d2178a3351021952a19949f
Amr Gamal <44068518+AmrGamal126@users.noreply.github.com>
2020-03-23 11:16:04 +0200

    Update nonsoftmax.sv

--- interface_top/nonsoftmax.sv
+++ interface_top/nonsoftmax.sv
@@ -5,21 +5,25 @@
 module nonsoftmax (
 	input clk,
 	input en_softmax,
-	output [9:0] prediction,
+	output reg [9:0] prediction,
 	input signed [15:0] fm 
 );
 
 reg [9:0] counter ; 
 reg [9:0] index ; 
+reg flag;
+reg nonsoftmax_end;
 reg signed [15:0] comp_feedback ;
 initial begin
    counter=0;
    index=0;
    comp_feedback=0;
+   flag=0;
+   nonsoftmax_end=0;
 end
 always @(posedge clk) begin
 	if(en_softmax)begin
-            if ( fm > comp_feedback ) begin
+            if ( fm > comp_feedback &&!flag) begin
                 comp_feedback <= fm ;
                 index <= counter  ;
                 counter <= counter +1 ;
@@ -27,6 +31,10 @@ always @(posedge clk) begin
             else
                 counter <= counter +1 ;
         end
+        if(counter>1022)begin
+            prediction<=index;
+            flag<=1;
+            nonsoftmax_end<=1;
+        end
    end     
-assign prediction  = index ;
 endmodule
